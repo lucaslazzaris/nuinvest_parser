32e341056b4f5825a8e0294c95502e2868da2298
LucasLazzaris <lucascercal.l@gmail.com>
2021-09-10 17:49:37 -0300

    Do not instantiate app

--- app.py
+++ app.py
@@ -49,8 +49,3 @@ def create_app(test_config=None):
     return render_template('about.html')
   
   return app
-
-if __name__ == '__main__':
-    app = create_app()
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
