8be1be0d23c3acc6841fc577bdfdc7669e6286d0
LucasLazzaris <lucascercal.l@gmail.com>
2021-09-15 22:53:52 -0300

    Refactor Home to use redux

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,42 +1,84 @@
-import React from 'react'
+import axios from 'axios';
+import { useCallback, useState } from 'react';
+import { connect } from 'react-redux';
+import { emitFlashMessage } from '../actions'
+import history from '../history'
+import StockContent from './StockContent'
 
-export default function Home() {
+function Home({emitFlashMessage}) {
+  const [stockData, setStockData] = useState(null);
+  const onSubmit = useCallback((event) => {
+    event.preventDefault();
+
+    const formData = new FormData(document.getElementById('files-form'));
+    axios
+      .post('processar_nota', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+      .then(({data}) => {
+        if (data.redirect_url) {
+          history.push(data.redirect_url);
+        } else {
+          setStockData(data.stocks);
+          if(data.flash_message){
+            emitFlashMessage(data.flash_message);
+          }
+        }
+      }
+      ).catch(({response}) => {
+        history.push(response.data.redirect_url);
+        emitFlashMessage(response.data.flash_message);
+        setStockData(null);
+      });
+  }, [emitFlashMessage]);
+
   return (
-  <>
-    <div class="p-5 mt-3 mb-3 bg-light rounded-3">
-      <h1 class="text-center">Leitor de notas de corretagem da Nu Invest</h1>
-      <form
-        id="files-form"
-        enctype="multipart/form-data"
-        action="/processar_nota"
-        method="post"
-      >
-        <div class="row justify-content-center">
-          <div class="col text-center">
-            <label class="form-label" for="files">Selecione os arquivos</label>
+    <>
+      <div className="p-5 mt-3 mb-3 bg-light rounded-3">
+        <h1 className="text-center">Leitor de notas de corretagem da Nu Invest</h1>
+        <form
+          id="files-form"
+          encType="multipart/form-data"
+          action="/processar_nota"
+          method="post"
+          onSubmit={onSubmit}
+        >
+          <div className="row justify-content-center">
+            <div className="col text-center">
+              <label className="form-label" htmlFor="files">
+                Selecione os arquivos
+              </label>
+            </div>
           </div>
-        </div>
-        <div class="row justify-content-center">
-          <div class="col-8">
-            <p>
-              <input
-                class="form-control"
-                type="file"
-                id="files"
-                name="files[]"
-                accept=".pdf"
-                multiple
-              />
-            </p>
+          <div className="row justify-content-center">
+            <div className="col-8">
+              <p>
+                <input
+                  className="form-control"
+                  type="file"
+                  id="files"
+                  name="files[]"
+                  accept=".pdf"
+                  multiple
+                />
+              </p>
+            </div>
           </div>
-        </div>
-        <div class="d-grid col-2 mx-auto">
-            <button class="btn btn-primary" type="submit">Enviar</button>
-        </div>
-      </form>
-    </div>
+          <div className="d-grid col-2 mx-auto">
+            <button className="btn btn-primary" type="submit">
+              Enviar
+            </button>
+          </div>
+        </form>
+      </div>
 
-    <div class="stocks-content hidden"></div>
-  </>
-  )
+      {stockData ? (
+        <StockContent stockData={stockData} />
+      ) : null}
+    </>
+  );
 }
+
+export default connect(null, {emitFlashMessage})(Home);
